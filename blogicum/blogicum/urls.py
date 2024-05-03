45ff94fbc2ff7bc1e67b10b86b054924a0794528
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-05-03 13:07:14 +0300

    Revised project

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path, include, reverse_lazy
+from django.urls import path, include
 
 from blog.views import RegistrationCreateView
 
